92d08720c423996c81ac0b069a9ac6ee4d6dedd3
aslan <aslanqaribov7@gmail.com>
2023-05-08 10:54:20 +0400

    second

---
+++ NestApp/NestApp/Areas/AdminPanel/Controllers/CategoriesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NestApp.DAL;
+
+namespace NestApp.Areas.AdminPanel.Controllers
+{
+	[Area("NestAdmin")]    
+	
+	public class CategoriesController : Controller
+	{
+		private readonly AppDbContext _context;
+		public CategoriesController(AppDbContext context)
+		{
+			_context=context;
+		}
+		public async IActionResult Index()
+        {
+            View(await _context.Categories.Where(x=>x.IsDeleted==false).ToListAsync());
+        }
+    }
+}
---
+++ NestApp/NestApp/Areas/AdminPanel/Controllers/ProductController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NestApp.Areas.AdminPanel.Controllers
+{
+    [Area("NestAdmin")]
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
